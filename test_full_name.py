357da10282491d953775874bc7f99179d49be14c
Chris Miraflor <chris.j.miraflor@gmail.com>
2021-05-04 23:03:32 -0700

    added tests for name generation

--- test_full_name.py
+++ test_full_name.py
@@ -6,6 +6,16 @@ class Tester(unittest.TestCase):
   def test_name(self):
     result = full_name.generate("Chris", "Miraflor")
     self.assertEqual(result, "Chris Miraflor")
+  
+  # Test for a case with non-string inputs.
+  def test_nums(self):
+    with self.assertRaises(TypeError):
+      full_name.generate(1234, 5678)
+  
+  # Test for a case with too few inputs.
+  def test_str_nums(self):
+    with self.assertRaises(TypeError):
+      full_name.generate("Full Name")
 
 if __name__ == '__main__':
   unittest.main()
